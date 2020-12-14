f926eb0be45df5bf3ae29f3ff3d12fa5308c14e2
Pynshik <60570875+Pynshik@users.noreply.github.com>
2020-12-14 13:02:38 +0300

    Add middlewares
    Adding middlewares of authorization using tokens, ID check, adding an avatar, validation.

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,15 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require('../controllers/users.controller.js');
-// const logPath = require('../middlewares/logPath.middlewares');
+const auth = require('../middlewares/auth.middleware');
+const idExist = require('../middlewares/id.middleware.js');
+const multerMiddleware = require('../middlewares/multer.middleware');
+const validate = require('../middlewares/validation.middleware.js');
+const createUserScheme = require('../validation-schemes/user-create.scheme.js');
+const updateUserScheme = require('../validation-schemes/user-update.scheme.js');
 
 router
-    .get('/', controller.getAll)
-    .get('/:id', controller.get)
-    .post('/', controller.add)
-    .put('/:id', controller.update)
-    .delete('/:id', controller.delete)
-
-
+    .get('/', auth, controller.getAll)
+    .post('/', multerMiddleware, validate(createUserScheme), controller.add)
+    .post('/login', controller.login)
+    .get('/me', auth, controller.getMe)
+    .get('/:id', auth, idExist, controller.get)
+    .put('/:id', auth, idExist, multerMiddleware, validate(updateUserScheme), controller.update)
+    .delete('/:id', auth, idExist, controller.delete)
 
 module.exports = router;
